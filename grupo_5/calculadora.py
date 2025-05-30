b8e9f25688162c6989721364e98d495305fcacec
Alexandre Lima <alexandrea.m.lima@gmail.com>
2025-05-29 22:06:48 -0300

    Implementa as quatro operações básicas na calculadora

--- grupo_5/calculadora.py
+++ grupo_5/calculadora.py
@@ -1,1 +1,26 @@
-# somos de negocio
+def soma(a, b):
+    """
+    Retorna a soma de dois números.
+    """
+    return a + b
+
+def subtracao(a, b):
+    """
+    Retorna a subtração entre dois números.
+    """
+    return a - b
+
+def multiplicacao(a, b):
+    """
+    Retorna a multiplicação entre dois números.
+    """
+    return a * b
+
+def divisao(a, b):
+    """
+    Retorna a divisão entre dois números. Lança erro se b for zero.
+    """
+    if b == 0:
+        raise ValueError("Divisão por zero não é permitida.")
+    return a / b
+
