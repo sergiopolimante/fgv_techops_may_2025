21d47aa815f5fa45d52dd037e935c8f532a62571
Nilson Gomes <nilsonbsb@gmail.com>
2025-06-02 14:25:42 -0300

    Versao inicial da calculadora com soma

---
+++ grupo_3/calculate.py
@@ -0,0 +1,40 @@
+#calculadora 
+
+def calculate(num1, num2, operation):
+    if not isinstance(num1, (int, float)) or not isinstance(num2, (int, float)):
+        raise TypeError("Os parâmetros num1 e num2 devem ser numéricos.")
+
+    operations = {
+        '1': lambda x, y: x + y,  # Adição
+   }
+    return operations[operation](num1, num2)
+
+def calculator():
+    while True:
+        print("\nBem-vindo à calculadora!")
+        print("Selecione a operação desejada ou 'q' para sair:")
+        print("1 - Adição (+)")
+        opcao = input("Digite 1 para soma ou 'q' para sair: ").strip().lower()
+
+        if opcao == 'q':
+            print("Encerrando a calculadora...")
+            break
+
+        if opcao not in ['1']
+            print("Opção inválida!")
+            continue
+
+        try:
+            num1 = float(input("Digite o primeiro número: "))
+            num2 = float(input("Digite o segundo número: "))
+        except ValueError:
+            print("Erro: entrada inválida, digite números válidos.")
+            continue
+
+        try:
+            resultado = calculate(num1, num2, opcao)
+            print(f"Resultado = {resultado}")
+            input("\nPressione Enter para continuar...")
+
+if __name__ == '__main__':
+    calculator()
