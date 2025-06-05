aceeff98df8c44e6369d8d0bea3456820b3c06f0
Compito5 <alexandrea.m.lima@gmail.com>
2025-06-05 13:21:21 -0300

    feat: adiciona funções soma e subtracao (resolve issue #1)

---
+++ calculadora.py
@@ -0,0 +1,11 @@
+def soma(a, b):
+    """
+    Retorna a soma de dois números.
+    """
+    return a + b
+
+def subtracao(a, b):
+    """
+    Retorna a subtração do segundo número em relação ao primeiro.
+    """
+    return a - b
