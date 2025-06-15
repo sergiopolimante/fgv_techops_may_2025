8cb828cf974c7ad943975c3fc09eb58825dc9a7c
rinaldops <rinaldops@petrobras.com.br>
2025-06-15 16:23:21 -0300

    restabelecendo a função subtração

--- grupo_4/calculadora.py
+++ grupo_4/calculadora.py
@@ -1,6 +1,12 @@
 def soma(a, b):
     """Retorna a soma de dois números."""
     return a + b
+
+def subtracao(a, b):
+    """Retorna a subtração de dois números."""
+    return a - b
+
 def multiplicacao(a, b):
     """Retorna a multiplicação de dois números."""
     return a * b
+
