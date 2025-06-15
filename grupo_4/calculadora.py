b741184a34f001c7eea96251f91d30388fea8014
euclideseng <euclidesbm.junior@gmail.com>
2025-06-15 15:25:26 -0300

    adiciona arquivo multiplicacao

--- grupo_4/calculadora.py
+++ grupo_4/calculadora.py
@@ -1,7 +1,6 @@
 def soma(a, b):
     """Retorna a soma de dois números."""
     return a + b
-
-def subtracao(a, b):
-    """Retorna a subtração de dois números."""
-    return a - b
+def multiplicacao(a, b):
+    """Retorna a multiplicação de dois números."""
+    return a * b
