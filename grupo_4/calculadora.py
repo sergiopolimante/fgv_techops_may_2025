5bfb5739c0609474eafd8b724790bf0b171d144d
priscillagranja <priscillagranja@gmail.com>
2025-06-16 20:16:57 -0300

    inclui função divisão

--- grupo_4/calculadora.py
+++ grupo_4/calculadora.py
@@ -10,3 +10,9 @@ def multiplicacao(a, b):
     """Retorna a multiplicação de dois números."""
     return a * b
 
+def divisao(a, b):
+    """Retorna a divisão de dois números."""
+    if b == 0:
+        return "Erro: divisão por zero não é permitida."
+    else:
+        return a / b
