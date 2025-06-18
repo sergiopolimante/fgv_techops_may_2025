1d78df691c986696b95b2dd3a35be16924c6314b
Leandro Fernandes <lmfunesp@hotmail.com>
2025-06-17 22:08:28 -0300

    funcao subtracao

--- grupo_7/calculadora.py
+++ grupo_7/calculadora.py
@@ -1,3 +1,7 @@
 def soma(a, b):
     """Retorna a soma de dois números."""
     return a + b
+
+def subtracao(a, b):
+    """Retorna a subtração de dois números."""
+    return a - b
--- grupo_7/test_calculadora.py
+++ grupo_7/test_calculadora.py
@@ -5,3 +5,8 @@ def test_soma():
     assert soma(2, 3) == 5
     assert soma(-1, 1) == 0
     assert soma(0, 0) == 0
+
+def test_subtracao():
+    assert subtracao(5, 3) == 2
+    assert subtracao(0, 4) == -4
+    assert subtracao(-3, -2) == -1
